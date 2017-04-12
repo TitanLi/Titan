910cb68da73b15a869ca96db4807b2eede8079ed
vincent850706 <Lisheng0706@gmail.com>
2017-04-12 14:58:30 +0800

    Add request

--- node-js/modules/request/request.js
+++ node-js/modules/request/request.js
@@ -1,6 +1,6 @@
 var request = require('request');
-request('127.0.0.1:3000', function (error, response, body) {
+request('http://www.google.com', function (error, response, body) {
     if (!error && response.statusCode == 200) {
         console.log(body) // Print the google web page.
      }
-})
+});
