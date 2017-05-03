95fbe1443c3a653b7dcd2127aedd2b0f976a8d96
vincent850706 <Lisheng0706@gmail.com>
2017-05-03 19:13:26 +0800

    Add example

---
+++ node-js/framework/koa/example/file.js
@@ -0,0 +1,48 @@
+var logger = require('koa-logger');
+var serve = require('koa-static');
+var parse = require('co-busboy');
+var koa = require('koa');
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+
+var app = koa();
+
+// log requests
+app.use(logger());
+
+// custom 404
+
+app.use(function *(next){
+  yield next;
+  if (this.body || !this.idempotent) return;
+  this.redirect('/404.html');
+});
+
+// serve files from ./public
+app.use(serve(__dirname+'/public'));
+// handle uploads
+
+app.use(function *(next){
+  // ignore non-POSTs
+  if ('POST' != this.method) return yield next;
+
+  // multipart upload
+  var parts = parse(this);
+  var part ;
+
+  while (part = yield parts) {
+    console.log(os.tmpdir().toString());
+    // var stream = fs.createWriteStream(path.join(os.tmpdir(), Math.random().toString()));
+    var stream = fs.createWriteStream(path.join(__dirname+'/file', Math.random().toString()));
+    part.pipe(stream);
+    console.log('uploading %s -> %s', part.filename, stream.path);
+  }
+
+  this.redirect('/');
+});
+
+// listen
+
+app.listen(3000);
+console.log('listening on port 3000');
