39f238b600304ba6b705151755a65f0a0ef7aae0
vincent850706 <Lisheng0706@gmail.com>
2017-05-04 16:27:14 +0800

    Add mongodb

--- node-js/framework/koa/example/blog/app.js
+++ node-js/framework/koa/example/blog/app.js
@@ -1,3 +1,5 @@
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
 var render = require('./lib/render');
 var logger = require('koa-logger');
 var route = require('koa-route');
@@ -6,7 +8,15 @@ var koa = require('koa');
 var app = koa();
 
 // database
-var posts = [];
+// var posts = [];
+var db;
+
+MongoClient.connect("mongodb://localhost:27017/blog",function(err,pDb){
+  if(err){
+    return console.dir(err);
+  }
+  db = pDb;
+});
 
 // middleware
 app.use(logger());
@@ -19,26 +29,33 @@ app.use(route.post('/post', create));
 
 // route definitions
 function *list() {
-  this.body = yield render('list', { posts: posts });
+  var collection = db.collection('post');                           //選擇collection為post
+  var posts = yield collection.find().toArray();                    //將資料以陣列表示
+  this.body = yield render('list', { posts: posts });               //選擇list.html並將posts傳入
 }
 
 function *add() {
-  this.body = yield render('new');
+  this.body = yield render('new');                                  //選擇new.html
 }
 
 function *show(id) {
-  var post = posts[id];
-  if (!post) this.throw(404, 'invalid post id');
-  this.body = yield render('show', { post: post });
+  // var post = posts[id];
+  var collection = db.collection('post');                           //選擇collection為post
+  var posts = yield collection.find({_id:ObjectId(id)}).toArray();  //搜尋特定id的資料，並以陣列表示
+  var post = posts[0];                                              //選擇第一筆資料
+  if (!post) this.throw(404, 'invalid post id');                    //該筆資料不存在
+  this.body = yield render('show', { post: post });                 //選擇show.html並將post傳入
 }
 
 function *create() {
-  var post = yield parse(this);   //{ title: 'apple', body: 'apple' }
-  posts.push(post);               //[{ title: 'apple', body: 'apple' }]
-  var id = posts.length - 1;
-  var date = new Date();
-  post.created_at = date.toString();
-  post.id = id;
+  var post = yield parse(this);   //{ title: 'apple', body: 'apple' }     //接收html傳來的資料
+  // posts.push(post);               //[{ title: 'apple', body: 'apple' }]
+  // var id = posts.length - 1;
+  var date = new Date();                                            //取得日期
+  post.created_at = date.toString();                                //加入日期資料
+  // post.id = id;
+  var collection = db.collection('post');                           //選擇collection為post
+  var results = yield collection.insertMany([post],{w:1});          //新增一筆資料https://docs.mongodb.com/manual/reference/write-concern/
   this.redirect('/');
 }
 
