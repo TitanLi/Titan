627550ecd442229eb15d3eb9555f4accfdc2311e
vincent850706 <Lisheng0706@gmail.com>
2017-05-09 17:45:54 +0800

    Add commit

--- node-js/standard/module-exports/app.js
+++ node-js/standard/module-exports/app.js
@@ -2,10 +2,19 @@ var Data = require('./lib/introduction.js');
 var array = require('./lib/array.js');
 var method = require('./lib/method.js');
 var object = require('./lib/object.js');
+
+//產生物件
 var data = new Data('Titan',20);
 data.about();
+console.log(data.ans());
+
+//使用Array
 console.log(array[5]);
+
+//使用method
 method.name();
+
+//使用multi-method
 object.name();
 object.age();
 object.believe();
--- node-js/standard/module-exports/lib/introduction.js
+++ node-js/standard/module-exports/lib/introduction.js
@@ -1,7 +1,11 @@
+//以function實作class
 module.exports = function(name,age){
   this.name = name;
   this.age = age;
   this.about = () => {
     console.log(name+' is '+age+' years old');
   };
+  this.ans = () => {
+    return 'apple';
+  }
 };
