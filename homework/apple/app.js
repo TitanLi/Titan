fd0402e4f9174404809343fdbec01658ed601038
vincent850706 <Lisheng0706@gmail.com>
2017-05-27 12:26:50 +0800

    Add customer post

--- homework/apple/app.js
+++ homework/apple/app.js
@@ -2,6 +2,7 @@ var koa = require('koa');
 var route = require('koa-route');
 var logger = require('koa-logger');
 var serve = require('koa-static');
+var parse = require('co-body');
 var render = require('./lib/render');
 
 var app = koa();
@@ -13,6 +14,10 @@ app.use(route.get('/',index));
 app.use(route.get('/customer',customer));
 app.use(route.get('/project',project));
 app.use(route.get('/staff',staff));
+app.use(route.get('/staff/information',information));
+app.use(route.get('/staff/pay',pay));
+app.use(route.get('/staff/list',list));
+app.use(route.post('/customer',customerData));
 
 function * index(){
   this.body = yield render('index');
@@ -22,12 +27,42 @@ function * customer(){
   this.body = yield render('customer');
 }
 
+function * customerData(date){
+  var data = yield parse(this);
+  console.log(data);
+  this.redirect('/customer');
+}
+
 function * project(){
   this.body = yield render('project');
 }
 
 function * staff(){
-  this.body = yield render('staff');
+  this.body = yield render('staff',{title:{"t1":"id","t2":"name","t3":"sex","t4":"old","t5":"birthday"},
+                                    apple:[{"id":1,"name":"apple","sex":"男","old":18,"birthday":"85/07/06"},
+                                           {"id":2,"name":"apple","sex":"男","old":18,"birthday":"85/07/06"},
+                                           {"id":3,"name":"apple","sex":"男","old":18,"birthday":"85/07/06"}]});
+}
+
+function * information(){
+  this.body = yield render('staff',{title:{"t1":"id","t2":"name","t3":"sex","t4":"old","t5":"birthday"},
+                                    apple:[{"id":1,"name":"apple","sex":"男","old":18,"birthday":"85/07/06"},
+                                           {"id":2,"name":"apple","sex":"男","old":18,"birthday":"85/07/06"},
+                                           {"id":3,"name":"apple","sex":"男","old":18,"birthday":"85/07/06"}]});
+}
+
+function * pay(){
+  this.body = yield render('staff',{title:{"t1":"id","t2":"name","t3":"sex","t4":"old","t5":"birthday"},
+                                    apple:[{"id":4,"name":"apple","sex":"男","old":18,"birthday":"85/07/06"},
+                                           {"id":5,"name":"apple","sex":"男","old":18,"birthday":"85/07/06"},
+                                           {"id":6,"name":"apple","sex":"男","old":18,"birthday":"85/07/06"}]});
+}
+
+function * list(){
+  this.body = yield render('staff',{title:{"t1":"id","t2":"name","t3":"sex","t4":"old","t5":"birthday"},
+                                    apple:[{"id":7,"name":"apple","sex":"男","old":18,"birthday":"85/07/06"},
+                                           {"id":8,"name":"apple","sex":"男","old":18,"birthday":"85/07/06"},
+                                           {"id":9,"name":"apple","sex":"男","old":18,"birthday":"85/07/06"}]});
 }
 
 app.listen(3000);
