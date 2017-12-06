e0a835ee12ad31d97063d696c3ef7bfd01dcd1d5
TitanLi <Lisheng0706@gmail.com>
2017-12-06 11:13:03 +0800

    Add package.json scripts

---
+++ node-js/package/hello_world/app.js
@@ -0,0 +1,25 @@
+var koa = require('koa');
+var route = require('koa-route');
+var logger = require('koa-logger');
+var serve = require('koa-static');
+var json = require('koa-json');
+var bodyParser = require('koa-bodyparser');
+var render = require('./lib/render.js');
+
+var app = koa();
+
+app.use(json());
+app.use(logger());
+app.use(bodyParser());
+app.use(serve(__dirname+'/lib'));
+
+app.use(route.get('/',index));
+
+function * index(){
+  this.body = yield render('index');
+}
+
+
+app.listen(3000,function(){
+  console.log('listening on port 3000');
+});
