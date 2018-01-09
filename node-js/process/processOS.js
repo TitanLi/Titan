6a6f94f497f4c7dae65ee815bbd9bc69babab608
李汶昇 <lisheng0706@gmail.com>
2018-01-09 16:25:22 +0800

    Add process OS

---
+++ node-js/process/processOS.js
@@ -0,0 +1,14 @@
+//現在系統平台
+console.info('現在系統平台：');
+console.info(process.platform);
+console.info();
+
+//現在CPU架構
+console.info('現在CPU架構：');
+console.info(process.arch);
+console.info();
+
+//現在shell的環境變數
+console.info('現在shell的環境變數：');
+console.info(process.env);
+console.info();
