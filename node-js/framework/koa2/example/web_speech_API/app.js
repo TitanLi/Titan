20b037639b668babf84f0a9569bd021d2c899c25
李汶昇 <lisheng0706@gmail.com>
2018-01-16 09:25:42 +0800

    Add koa2 web speech api example

--- node-js/framework/koa2/example/web_speech_API/app.js
+++ node-js/framework/koa2/example/web_speech_API/app.js
@@ -1,26 +1,25 @@
 var koa = require('koa');
-var route = require('koa-route');
+var Router = require('koa-router');
 var logger = require('koa-logger');
 var serve = require('koa-static');
 var json = require('koa-json');
 var bodyParser = require('koa-bodyparser');
 var render = require('./lib/render.js');
 
-var app = koa();
-
+var app = new koa();
+var router = Router();
 app.use(json());
 app.use(logger());
 app.use(bodyParser());
 app.use(serve(__dirname+'/lib'));
 app.use(serve(__dirname+'/img'));
 
-app.use(route.get('/',index));
-
-function * index(){
-  this.body = yield render('index');
-}
+router.get('/',async function(ctx){
+  ctx.body = await render('index');
+});
 
 
+app.use(router.routes());
 app.listen(3000,function(){
   console.log('listening on port 3000');
 });
