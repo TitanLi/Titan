03f2090774b7ffd2c54122505788f230fe8adbd6
李汶昇 <lisheng0706@gmail.com>
2018-01-16 11:35:03 +0800

    Add itri-tts

---
+++ node-js/framework/koa2/example/web_speech_API/test/app.js
@@ -0,0 +1,21 @@
+const TTSClient = require('itri-tts');
+const account = require('./config.json');
+var tts = new TTSClient(account.accountID, account.password);
+
+var text = '您好，我是Bruce，感謝您使用工研院文字轉語音Web服務。'
+
+// tts.ConvertSimple(text, function (err, result) {
+//   if (err) throw err
+//   console.log(result)  // { resultCode: '0',
+//                        //   resultString: 'success',
+//                        //   resultConvertID: CONVERT_ID }
+// })
+
+tts.GetConvertStatus(4914813, function (err, result) {
+  if (err) throw err
+  console.log(result)  // { resultCode: '0',
+                       //   resultString: 'success',
+                       //   statusCode: '2',
+                       //   status: 'completed',
+                       //   resultUrl: AUDIO_ADDRESS }
+})
