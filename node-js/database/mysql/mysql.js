cfe3705c817178855354585eab0de398ad03ecd0
TitanLi <Lisheng0706@gmail.com>
2018-12-26 20:02:27 +0800

    Add mysql example

--- node-js/database/mysql/mysql.js
+++ node-js/database/mysql/mysql.js
@@ -1,15 +1,15 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : 'apple'
+  host: 'localhost',
+  user: 'root',
+  password: 'apple'
 });
 
-connection.connect(function(err){
-  if(err)
-    throw err;
-
-  console.log('Connected');
-  connection.end();
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
 });
